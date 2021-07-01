5f0faa4fd38127b398ad69f474523eb3af5f0bb9
Matheus <mathlouly@gmail.com>
2021-07-01 11:21:35 -0300

    Remove clear console

--- connections/shared/clear_console.dart
+++
@@ -1,7 +0,0 @@
-import 'dart:io';
-
-void ClearConsole() {
-  for (var i = 0; i < 100; i++) {
-    stdout.writeln();
-  }
-}
--- connections/socket/client.dart
+++ connections/socket/client.dart
@@ -1,11 +1,8 @@
 import 'dart:io';
 
-import '../shared/clear_console.dart';
-
 void main(List<String> args) async {
   while (true) {
     await Socket.connect('localhost', 8000).then((Socket socket) {
-      ClearConsole();
       stdout.write('Write u message > ');
       String? msg = stdin.readLineSync();
       socket.write(msg ?? '');
